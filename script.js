9894961a7fce24cc5fd376e26ba8b83847b32ae7
Vedant Parikh <vkp236@nyu.edu>
2016-07-29 00:08:06 -0400

    Added functions as abstraction

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-// Code goes here
+/*
+Functions as abstractions - Important concept of angularjs
+var work = function() {
+	console.log("working hard");
+};
+
+var dowork = function(f){
+	console.log("Started working");
+	try{
+		f();
+	}
+	catch(ex){
+		console.log(ex);
+	}
+};
+
+dowork(work);
+*/
 
