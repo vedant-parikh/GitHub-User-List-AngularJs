3e5d8cdc33a23093ad5c51ca5d29aa6055d76002
Vedant Parikh <vkp236@nyu.edu>
2016-07-29 00:14:25 -0400

    Added functions as modules.

--- script.js
+++ script.js
@@ -15,5 +15,30 @@ var dowork = function(f){
 };
 
 dowork(work);
+
+Functions to build modules.
+
+Functions to avoid global variables.
+
+var createWorker = function(){
+
+	var task1 = function(){
+		console.log("Task1");
+	};
+	var task2 = function(){
+		console.log("Task2");
+	};
+	return {
+		job1: task1,
+		job2: task2
+	}
+};
+
+
+var worker = createWorker();
+worker.job1();
+worker.job2();
 */
+
+
 
