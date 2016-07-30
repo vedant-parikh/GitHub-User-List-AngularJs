eb62d94bad77ccd7c96a5b206e9153bf4fdb3957
Vedant Parikh <vkp236@nyu.edu>
2016-07-30 01:41:04 -0400

    Completed creating required directives.

--- script.js
+++ script.js
@@ -43,33 +43,56 @@ worker.job2();
 }());
 
 Functions to avoid global variables.
-
-
 */
-(function() {
-	angular.module("githubViewer", []).controller("MainController", ["$scope", "$http", MainController]);
-	//MainController.$inject = ["$scope","$http"];
-	function MainController($scope, $http){
 
+(function() {
+	angular.module("githubViewer", []).controller("MainController", ["$scope", "$http", "$interval", "$log", MainController]);
+	//MainController.$inject = ["$scope","$http"];
+	function MainController($scope, $http, $interval, $log){
+		
 		var onUserComplete = function(response){
 			$scope.user = response.data;
 			$http.get($scope.user.repos_url).then(onRepos, onReposError);
 		}
-
+		
 		var onError = function(reason){
 			$scope.error = "Could not fetch the user"
 		}
+		
 		var onRepos = function(response){
 			$scope.repos = response.data;
 		}
+		
 		var onReposError = function(reason){
 			$scope.error = "Could not fetch the Repository"
 		}
-		$scope.search = function(username) {
-			$http.get("https://api.github.com/users/" + username).then(onUserComplete, onError);
+		
+		var decrementCountdown= function(){
+			$scope.countdown -= 1;
+			if($scope.countdown < 1) {
+				$scope.search($scope.username);
+			}
 		}
+		
+		var countdownInterval = null;
+		var startCountdown = function(){
+			countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+		}
+		
+		$scope.search = function(username) {
+			$log.info("searching for " + username);
+			$http.get("https://api.github.com/users/" + username).then(onUserComplete, onError);
+			if(countdownInterval){
+				$interval.cancel(countdownInterval);
+				$scope.countdown = null;
+			}
+		}
+		
 		$scope.repoOrderBy = "+stargazers_count";
 		$scope.message = "Github Viewer";
 		$scope.username = "Angular";
+		$scope.countdown = 5;
+		
+		startCountdown();
 	}
 })();
