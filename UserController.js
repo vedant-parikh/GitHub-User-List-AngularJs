f0098dab6716e23addd871a5094e2e65d9b9db43
Vedant Parikh <vkp236@nyu.edu>
2016-07-31 13:26:02 -0400

    Created routers for the application

--- UserController.js
+++ UserController.js
@@ -1,52 +1,5 @@
-/*
-Functions as abstractions - Important concept of angularjs
-var work = function() {
-	console.log("working hard");
-};
-
-var dowork = function(f){
-	console.log("Started working");
-	try{
-		f();
-	}
-	catch(ex){
-		console.log(ex);
-	}
-};
-
-dowork(work);
-
-REVEALING MODULE PATTERNS: Functions to build modules.
-
-# Do not make global variables, They are downright evil.
-
-# Immediately invoked function expression
-(var program = function() {
-var createWorker = function(){
-
-	var task1 = function(){
-		console.log("Task1");
-	};
-	var task2 = function(){
-		console.log("Task2");
-	};
-	return {
-		job1: task1,
-		job2: task2
-	}
-};
-
-
-var worker = createWorker();
-worker.job1();
-worker.job2();
-}());
-
-Functions to avoid global variables.
-*/
-
 (function() {
-	var app = 	angular.module("githubViewer", []);
+	var app = 	angular.module("githubViewer");
 	//MainController.$inject = ["$scope","$http"];
 	
 	var MainController = function($scope, github, $interval, $log, $location, $anchorScroll){
